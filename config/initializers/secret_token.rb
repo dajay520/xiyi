1603c56282a266ad707c9cd72990bed08f4c1375
dajay <dajay520@qq.com>
2011-12-18 19:26:42 -0800

    commit all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xiyi::Application.config.secret_token = '073a81a21a6ac8a14250ca310b22502acd561241d0f4783f7ffbe99d2f15e63e8ceb63538a6017d976db8e851ffca03cbd0a1b8b4670d4209c6c54ab2e459ff4'
